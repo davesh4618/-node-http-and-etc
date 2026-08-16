73e88fbe277a2e27c5b6d8ccf123039519c4f617
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-08-16 19:05:06 +0530

    delete functionality

--- airbnb/models/home.model.js
+++ airbnb/models/home.model.js
@@ -1,35 +1,58 @@
-let { houseData } = require("../utility/housestore");
 const fs = require("fs");
 const path = require("path");
 const rootdir = require("../utility/path_utils");
-const { stringify } = require("querystring");
-const { json } = require("body-parser");
+
 module.exports = class Home {
-  constructor(housename, price, location, rating) {
-    ((this.housename = housename),
-      (this.price = price),
-      (this.location = location),
-      (this.rating = rating));
+  constructor(housename, price, location, rating, id) {
+    this.housename = housename;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    if (id) this.id = id;
   }
+
   save() {
-    this.id = Math.random().toString();
-    houseData.push(this);
-    const pathfordata = path.join(rootdir, "data", "housedata.json");
-    fs.writeFile(pathfordata, JSON.stringify(houseData), (err) => {
-      console.log(err);
-    });
-  }
-  static fetchall(callback) {
     const pathfordata = path.join(rootdir, "data", "housedata.json");
 
+    Home.fetchall((houseData) => {
+      if (this.id) {
+        const existingHomeIndex = houseData.findIndex(
+          (home) => home.id === this.id
+        );
+        if (existingHomeIndex >= 0) {
+          houseData[existingHomeIndex] = this;
+        } else {
+          houseData.push(this);
+        }
+      } else {
+        this.id = Math.random().toString();
+        houseData.push(this);
+      }
+
+      fs.writeFile(pathfordata, JSON.stringify(houseData), (err) => {
+        if (err) console.log(err);
+      });
+    });
+  }
+
+  static fetchall(callback) {
+    const pathfordata = path.join(rootdir, "data", "housedata.json");
     fs.readFile(pathfordata, (err, data) => {
       if (err) {
         callback([]);
         return;
       }
+      callback(JSON.parse(data.toString()));
+    });
+  }
 
-      const houseData = JSON.parse(data.toString());
-      callback(houseData);
+  static deletebyid(id) {
+    const pathfordata = path.join(rootdir, "data", "housedata.json");
+    Home.fetchall((houseData) => {
+      const updatedHouseData = houseData.filter((home) => home.id !== id);
+      fs.writeFile(pathfordata, JSON.stringify(updatedHouseData), (err) => {
+        if (err) console.log(err);
+      });
     });
   }
 };
--- airbnb/routes/hostrouter.js
+++ airbnb/routes/hostrouter.js
@@ -5,9 +5,12 @@ const path = require("path");
 const { addhome } = require("../controler/home");
 const { getedithome } = require("../controler/home");
 const { adddata } = require("../controler/home");
+const { postedithome } = require("../controler/home");
+const { deletehome } = require("../controler/home");
 hostrouter.get("/add-home", addhome);
 
 hostrouter.post("/add-home", adddata);
 hostrouter.get("/edit-homes/:id", getedithome);
-
+hostrouter.post("/edit-homes/:id", postedithome);
+hostrouter.post("/delete-home/:id", deletehome);
 exports.hostrouter = hostrouter;
