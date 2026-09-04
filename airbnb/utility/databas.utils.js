d3034b01a3bf6f192281a843ae792cfb52523521
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-09-04 16:04:58 +0530

    mmmm

--- airbnb/utility/databas.utils.js
+++ airbnb/utility/databas.utils.js
@@ -1,15 +1,20 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const url ="mongodb+srv://devesh:devesh123@cluster0.7kgephf.mongodb.net/?appName=Cluster0"
+const url =
+  "mongodb+srv://devesh:<password>@cluster0.7kgephf.mongodb.net/?appName=Cluster0";
 
-const mongoconnect = (callback) =>{
+const mongoconnect = (callback) => {
+  let db;
+  MongoClient.connect(url)
+    .then((client) => {
+      console.log("Connected to MongoDB");
+      db = client.db("airbnb");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB", err);
+    });
+};
 
-MongoClient.connect(url).then(client =>{
-  console.log("Connected to MongoDB");
-  callback(client);
-}).catch(err =>{
-  console.log("Failed to connect to MongoDB", err);
-})
-}
 module.exports = mongoconnect;
