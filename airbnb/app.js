331e5ff39ee21c29f7204062965364071d208348
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-09-19 19:28:58 +0530

    add authentication

--- airbnb/app.js
+++ airbnb/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const rootpath = require("./utility/path_utils");
 const userrouter = require("./routes/userroutes");
 const { hostrouter } = require("./routes/hostrouter");
-
+const authrouter = require("./routes/authrouter.js");
 const { pagenotfound } = require("./controler/errors");
 
 const { default: mongoose } = require("mongoose");
@@ -13,14 +13,15 @@ const { default: mongoose } = require("mongoose");
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(rootpath, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(userrouter);
-app.use(express.urlencoded());
-app.use("/host", hostrouter);
 
+app.use("/host", hostrouter);
+app.use(authrouter);
 app.use(pagenotfound);
 
 const dbpath =
-  "mongodb+srv://devesh:@cluster0.7kgephf.mongodb.net/airbnb?appName=Cluster0";
+  "mongodb+srv://devesh:*******@cluster0.7kgephf.mongodb.net/airbnb?appName=Cluster0";
 
 mongoose
   .connect(dbpath)
