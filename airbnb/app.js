2e982a08cb840dd27ed3e40865faa65bd6c5b709
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-09-21 12:49:27 +0530

    logout controler

--- airbnb/app.js
+++ airbnb/app.js
@@ -12,16 +12,30 @@ const { default: mongoose } = require("mongoose");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
 app.use(express.static(path.join(rootpath, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log("cookie", req.get("Cookie"));
+  req.isloggedin = req.get("Cookie")?.split("=")[1] === "true" || false;
+  next();
+});
 app.use(userrouter);
 
+app.use("/host", (req, res, next) => {
+  if (!req.isloggedin) {
+    return res.redirect("/login");
+  } else {
+    next();
+  }
+});
 app.use("/host", hostrouter);
 app.use(authrouter);
 app.use(pagenotfound);
 
 const dbpath =
-  "mongodb+srv://devesh:devesh123@cluster0.7kgephf.mongodb.net/airbnb?appName=Cluster0";
+  "mongodb+srv://devesh:.......@cluster0.7kgephf.mongodb.net/airbnb?appName=Cluster0";
 
 mongoose
   .connect(dbpath)
--- airbnb/routes/authrouter.js
+++ airbnb/routes/authrouter.js
@@ -3,7 +3,8 @@ const authrouter = express.Router();
 
 const { authcontroler } = require("../controler/authcontroler.js");
 const { authpostcontroler } = require("../controler/authcontroler.js");
+const { logoutconstroler } = require("../controler/authcontroler.js");
 authrouter.get("/login", authcontroler);
 authrouter.post("/login", authpostcontroler);
-
+authrouter.post("/logout", logoutconstroler);
 module.exports = authrouter;
