22dbd171830a3efa16935d219775652173948057
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-09-25 16:27:44 +0530

    express validator

--- airbnb/controler/authcontroler.js
+++ airbnb/controler/authcontroler.js
@@ -1,3 +1,4 @@
+const { check, validationResult } = require("express-validator")
 exports.authcontroler = (req, res, next) => {
   res.render("auth/login", { pagetittle: "Login", isloggedin: false });
 };
@@ -8,10 +9,42 @@ exports.authpostcontroler = (req, res, next) => {
   res.redirect("/");
 };
 exports.logoutconstroler = (req, res, next) => {
-  res.clearCookie("isloggedin");
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error destroying session:", err);
+    }
+  });
   console.log("cookie cleared");
   res.redirect("/");
 };
 exports.signupcontroler = (req, res, next) => {
-  res.render("auth/signup", { pagetittle: "Signup", isloggedin: false });
+  res.render("auth/signup", { pagetittle: "Signup", 
+    errors :[],
+    output : { firstName: "", lastname: "", email: "", password: "", confirmPassword: "" }
+    ,isloggedin: false });
 };
+exports.signuppostcontroler = [
+  check("firstName").notEmpty().withMessage("First name is required"),
+  check("lastname").notEmpty().withMessage("Last name is required"),
+  check("email").isEmail().withMessage("Please enter a valid email address"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  check("confirmPassword")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match"),
+  (req, res, next) => {
+    const { firstName, lastname, email, password, confirmPassword } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pagetittle: "Signup",
+        isloggedin: false,
+        errors: errors.array().map((err) => err.msg),
+        output: { firstName, lastname, email, password, confirmPassword },
+      });
+    }
+
+   res.redirect("/logout")
+  },
+];
--- airbnb/routes/authrouter.js
+++ airbnb/routes/authrouter.js
@@ -5,8 +5,11 @@ const { authcontroler } = require("../controler/authcontroler.js");
 const { authpostcontroler } = require("../controler/authcontroler.js");
 const { logoutconstroler } = require("../controler/authcontroler.js");
 const { signupcontroler } = require("../controler/authcontroler.js");
+const { signuppostcontroler } = require("../controler/authcontroler.js");
 authrouter.get("/login", authcontroler);
 authrouter.post("/login", authpostcontroler);
 authrouter.post("/logout", logoutconstroler);
 authrouter.get("/signup", signupcontroler);
+authrouter.post("/signup", signuppostcontroler);
 module.exports = authrouter;
+ 
